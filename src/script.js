8bd62177282023a6f4496354eaf30ac42522de95
Mohith J <mohithjegan@gmail.com>
2026-03-05 16:17:24 -0500

    Add JavaScript logic

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// variable
+const projectName = "WDDM126 Final Project";
+
+// function
+function showProjectInfo() {
+  console.log("Project Name:", projectName);
+}
+
+// call function
+showProjectInfo();
